45292400fc732d440a913e330dd5b11b2847c4b7
timnasa999 <Timotheomgaya@gmail.com>
2025-01-21 00:46:08 +0300

    Update aliv.js

--- commandes/aliv.js
+++ commandes/aliv.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test1", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*Hello* 👋 \n\n ' + "i'm a whatsapp bot multi-device created by Boniphace Tech";
-    let d = '                                                                           𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐁𝐎𝐍𝐈𝐏𝐇𝐀𝐂𝐄 𝐓𝐄𝐀𝐌✨';
+    let z = '*Hello* 👋 \n\n ' + "i'm a whatsapp bot multi-device created by Timnasa Tech";
+    let d = '                                                                  venom or      𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐓𝐈𝐌𝐍𝐀𝐒𝐀 𝐓𝐄𝐀𝐌✨';
     let varmess = z + d;
-    var mp4 = 'https://files.catbox.moe/u41d8b.jpeg';
+    var mp4 = 'https://files.catbox.moe/gggs3h.jpg';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
